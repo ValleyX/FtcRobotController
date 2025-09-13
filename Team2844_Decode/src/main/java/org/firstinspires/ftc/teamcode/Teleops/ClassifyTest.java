a2978128e4a0c81f37e9b1beb6c3176b18d4d27f
Josh Ettinger <Josh_Ettinger@yahoo.com>
2025-09-13 10:02:19 -0700

    Prototype test for 2844

---
+++ Team2844_Decode/src/main/java/org/firstinspires/ftc/teamcode/Teleops/ClassifyTest.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.Team2844_Decode.src.main.java.org.firstinspires.ftc.teamcode.Teleops;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+@TeleOp(name="Prototype Test")
+public class ClassifyTest extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive()) {
+
+        }
+    }
+}
