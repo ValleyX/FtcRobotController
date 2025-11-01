ba3488ea7fd91ee67df034e4ec889cde5c036f7e
Josh Ettinger <Josh_Ettinger@yahoo.com>
2025-11-01 09:53:04 -0700

    Working on roadrunner

--- Team2844/src/main/java/org/firstinspires/ftc/team2844/Team2844_Decode/RoadrunnerStuff/RoadrunnerQuickstart/MecanumDrive.java
+++ Team2844/src/main/java/org/firstinspires/ftc/team2844/Team2844_Decode/RoadrunnerStuff/RoadrunnerQuickstart/MecanumDrive.java
@@ -63,13 +63,13 @@ public final class MecanumDrive {
                 RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD;
 
         // drive model parameters
-        public double inPerTick = 1;
+        public double inPerTick = 70/35733.6;
         public double lateralInPerTick = inPerTick;
         public double trackWidthTicks = 0;
 
         // feedforward parameters (in tick units)
-        public double kS = 0;
-        public double kV = 0;
+        public double kS = 0.6619220245093267;
+        public double kV = 0.00027033396733482215;
         public double kA = 0;
 
         // path profile parameters (in inches)
