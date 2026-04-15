23aa73f822b212720c593e23af9a1550550e1749
Josh Ettinger <Josh_Ettinger@yahoo.com>
2026-04-14 17:14:16 -0700

    Zamboni after states, just moving files

--- Team2844/src/main/java/org/firstinspires/ftc/team2844/Team2844_Decode/CommandBased/SubSystems/DriveSubsystems/DriveSubsystem.java
+++ Team2844/src/main/java/org/firstinspires/ftc/team2844/Team2844_Decode/CommandBased/SubSystems/DriveSubsystems/DriveSubsystem.java
@@ -2,19 +2,14 @@ package org.firstinspires.ftc.team2844.Team2844_Decode.CommandBased.SubSystems.D
 
 import com.acmerobotics.roadrunner.Pose2d;
 import com.arcrobotics.ftclib.command.SubsystemBase;
-import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.team2844.Team2844_Decode.CommandBased.Helper.Constants;
 import org.firstinspires.ftc.team2844.Team2844_Decode.CommandBased.Helper.SavedVars;
-import org.firstinspires.ftc.team2844.Team2844_Decode.CommandBased.Roadrunner.MecanumDrive;
+import org.firstinspires.ftc.team2844.Team2844_Decode.CommandBased.OldAutos.Roadrunner.MecanumDrive;
 
 import java.util.function.DoubleSupplier;
-import java.util.function.Supplier;
 
 //import org.firstinspires.ftc.team2844.Team2844_Decode.RoadrunnerStuff.RoadrunnerQuickstart.MecanumDrive;
 
