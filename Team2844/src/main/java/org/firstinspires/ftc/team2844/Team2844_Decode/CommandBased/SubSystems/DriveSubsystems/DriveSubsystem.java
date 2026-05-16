5952e52005f05815b7a99435de9909a148e1500d
Josh Ettinger <joshe@juno.com>
2026-05-15 17:16:24 -0700

    Working on Zamboni, tracking and intake/shooting

--- Team2844/src/main/java/org/firstinspires/ftc/team2844/Team2844_Decode/CommandBased/SubSystems/DriveSubsystems/DriveSubsystem.java
+++ Team2844/src/main/java/org/firstinspires/ftc/team2844/Team2844_Decode/CommandBased/SubSystems/DriveSubsystems/DriveSubsystem.java
@@ -157,7 +157,15 @@ public class DriveSubsystem extends SubsystemBase {
         double botX = getBotX();
         double botY = getBotY();
         double angle = 0.0;
-        double tempHeading = getRobotHeading();
+        double tempHeading = getHeadingFlipped();
+
+        tempHeading += 180.0;
+        if(tempHeading > 360.0){
+            tempHeading -= 360.0;
+        } else if (tempHeading < 0.0){
+            tempHeading += 360.0;
+        }
+
 
         double limelightX = 0.0;
         double limelightY = 0.0;
@@ -166,15 +174,13 @@ public class DriveSubsystem extends SubsystemBase {
         if(pipeline == Constants.BLUE_PIPELINE || pipeline == Constants.BLUE_PIPELINE_MOTIF){
             limelightX = Constants.BLUE_APRILTAG_X;
             limelightY = Constants.BLUE_APRILTAG_Y;
-            tempHeading = tempHeading;
         } else if(pipeline == Constants.RED_PIPELINE || pipeline == Constants.RED_PIPELINE_MOTIF){
             limelightX = Constants.RED_APRILTAG_X;
             limelightY = Constants.RED_APRILTAG_Y;
-            tempHeading = -tempHeading;
         }
 
-        opposite = limelightX - botX;
-        adjacent = limelightY - botY;
+        opposite = limelightX + botX;
+        adjacent = limelightY + botY;
 
         angle = Math.toDegrees(Math.atan2(adjacent, opposite));
 
